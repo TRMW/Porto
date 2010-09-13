02e4634dc99a1a89b8469902ad4735a2d819b606
Matt Wright <Matt@Kristens-MacBook.local>
2010-09-12 19:36:50 -0700

    added s3

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,9 @@
 class Photo < ActiveRecord::Base
   belongs_to :portfolio	
-  has_attached_file :image, :styles => { :medium => "720x576>", :thumb => "200x200>" }, :default_style => :default
+  has_attached_file :image,
+  									:styles => { :medium => "720x576>", :thumb => "200x200>" },
+  									:default_style => :default,
+  									:storage => :s3, 
+      							:s3_credentials => "#{RAILS_ROOT}/config/s3.yml", 
+      							:path => "/:style/:filename"
 end
