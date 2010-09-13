3c1bdbdf7539b0021e379c9a1d1a8bdd94ffcd20
Matt on Kristen's MacBook <wrattmight@gmail.com>
2010-09-12 19:55:21 -0700

    working on photo uploads

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,7 @@
 class Photo < ActiveRecord::Base
   belongs_to :portfolio	
   has_attached_file :image,
-  									:styles => { :medium => "720x576>", :thumb => "200x200>" },
+  									:styles => { :default => "720x576>", :thumb => "200x200>" },
   									:default_style => :default,
   									:storage => :s3, 
       							:s3_credentials => "#{RAILS_ROOT}/config/s3.yml", 
