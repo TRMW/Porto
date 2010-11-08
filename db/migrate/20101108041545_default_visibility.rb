a74746f8b68faba2e5f43d71915c88b7399a79a9
Matt Wright <wrattmight@gmail.com>
2010-11-07 23:29:19 -0800

    trying to fix migrations

--- db/migrate/20101108041545_default_visibility.rb
+++ db/migrate/20101108041545_default_visibility.rb
@@ -1,5 +1,6 @@
 class DefaultVisibility < ActiveRecord::Migration
   def self.up
+  	add_index :settings, :var, :unique => true
   	change_column_default :portfolios, :visible, 'f'
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101108041545) do
+ActiveRecord::Schema.define(:version => 20101107193136) do
 
   create_table "photos", :force => true do |t|
     t.string    "image_file_name"
@@ -21,28 +21,19 @@ ActiveRecord::Schema.define(:version => 20101108041545) do
   end
 
   create_table "portfolios", :force => true do |t|
-    t.string   "title"
-    t.boolean  "visible",    :default => false
-    t.integer  "position"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string    "title"
+    t.boolean   "visible",    :default => false
+    t.integer   "position"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   create_table "posts", :force => true do |t|
-    t.string   "title"
-    t.text     "body"
-    t.string   "image_file_name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string    "title"
+    t.text      "body"
+    t.string    "image_file_name"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
-
-  create_table "settings", :force => true do |t|
-    t.string   "var",        :null => false
-    t.text     "value"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "settings", ["var"], :name => "index_settings_on_var"
 
 end
