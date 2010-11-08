6de4fa7cd8198308863e8ae55b6dd257aaeca86e
Matt Wright <wrattmight@gmail.com>
2010-11-08 00:39:49 -0800

    re-adding get_portfolios in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-#   before_filter :get_portfolios
-#   
-#   def get_portfolios
-# 		@portfolios = Portfolio.all(:order => 'position')
-# 	end
+  before_filter :get_portfolios
+  
+  def get_portfolios
+		@portfolios = Portfolio.all(:order => 'position')
+	end
 end
