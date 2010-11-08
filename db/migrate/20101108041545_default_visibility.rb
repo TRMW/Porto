1192b35485ca0771f6b55a01baefbf04a46ee1c2
Matt Wright <wrattmight@gmail.com>
2010-11-07 23:16:12 -0800

    turning off get_portfolios in app controller while i build the first one

--- db/migrate/20101108041545_default_visibility.rb
+++ db/migrate/20101108041545_default_visibility.rb
@@ -1,7 +1,6 @@
 class DefaultVisibility < ActiveRecord::Migration
   def self.up
   	change_column_default :portfolios, :visible, 'f'
-  	add_index :settings, :var, :unique => true
   end
 
   def self.down
