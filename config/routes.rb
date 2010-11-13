f5c858034ee7983db8dbd04286091af1ae6f1b1a
Matt Wright <wrattmight@gmail.com>
2010-11-13 14:38:35 -0800

    alias /posts as /news

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,17 @@ Dru::Application.routes.draw do
 
 	match 'portfolios/add_new_photos' => 'portfolios#add_new_photos', :as => :add_new_photos
 	match 'portfolios/set_position' => 'portfolios#set_position', :as => :set_position
-	match 'settings' => 'portfolios#settings'
 	match 'contact' => 'portfolios#contact'
 	match 'bio' => 'portfolios#bio'
 	
 	match '/universityclub' => 'portfolios#index', :as => :admin
-	scope '/universityclub', :as => "admin" do
+	scope '/universityclub', :as => 'admin' do
   	resources :portfolios, :except => :show
   	resources :posts, :except => [ :show, :index ]
 	end
 	
 	resources :portfolios, :only => :show
-  resources :posts, :only => [ :show, :index ]
+  resources :posts, :only => [ :show, :index ], :path => '/news'
 	
 	root :to => "portfolios#show", :id => Settings.front_portfolio
 	
