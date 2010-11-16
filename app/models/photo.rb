381656d863a9c4c41e391990f4cdbecdd02cd526
Matt Wright <wrattmight@gmail.com>
2010-11-15 22:43:54 -0800

    adding s3 alias

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -6,6 +6,8 @@ class Photo < ActiveRecord::Base
   									:styles => { :default => "720x576>", :thumb => "223x223>" },
   									:default_style => :default,
   									:storage => :s3, 
-      							:s3_credentials => "#{RAILS_ROOT}/config/s3.yml", 
+      							:s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+      							:s3_host_alias => "images.drudonovan.com",
+      							:url => ":s3_alias_url",
       							:path => "portfolios/:portfolio_id/:basename/:basename-:style.:extension"
 end
