cfc3cca39adac6c2cec4513d278f0434dee694af
Matt Wright <wrattmight@gmail.com>
2012-02-11 16:24:57 -0800

    remove semicolon in redirect statements in hopes that app will stop crashing on bamboo stack

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,6 @@
 class Portfolio < ActiveRecord::Base
   extend FriendlyId
-  friendly_id :title, use: [:slugged, :history]
+  friendly_id :title, :use => [:slugged, :history]
   validates_presence_of :title, :slug, :photos
   has_many :photos, :order => "position ASC", :dependent => :destroy
   accepts_nested_attributes_for :photos, :allow_destroy => true
