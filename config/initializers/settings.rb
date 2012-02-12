d7acb730fb9d3d83e87edb3d1989b14bf38b5d7a
Matt Wright <wrattmight@gmail.com>
2012-02-11 16:54:10 -0800

    fix Settings defaults

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,7 +1,8 @@
-Settings.defaults[:show_portfolios] = true
+Settings.defaults[:show_portfolios] = 'true'
+Settings.defaults[:front_portfolio] = Portfolio.first
 Settings.defaults[:news_title] = 'news'
 Settings.defaults[:bio_title] = 'bio'
 Settings.defaults[:contact_title] = 'contact'
-Settings.defaults[:show_news] = true
-Settings.defaults[:show_bio] = true
-Settings.defaults[:show_contact] = true
+Settings.defaults[:show_news] = 'true'
+Settings.defaults[:show_bio] = 'true'
+Settings.defaults[:show_contact] = 'true'
