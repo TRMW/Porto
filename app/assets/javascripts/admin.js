ac66d3891f16556138214ef274647f0dd2d910bc
Matt Wright <wrattmight@gmail.com>
2012-02-22 20:29:04 -0800

    add javascript to disable/enable section title inputs based on corresponding checkboxes

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -11,4 +11,36 @@ $(document).ready(function(){
       $(".main").before("<div id=\"notice\">Project order updated.</div>");
     }
   });
+  
+  // Disable title inputs for disabled sections of the site
+  if ( '.settings' ) {
+    var checks = [{
+        check: $('#show_blog'),
+        input: $('#blog_title')
+      },
+      {
+        check: $('#show_about'),
+        input: $('#about_title')
+      },
+      {
+        check: $('#show_contact'),
+        input: $('#contact_title')
+      }];
+    
+    $(checks).each(function(){
+      var current = this;
+      if ( !current.check.attr('checked') ) {
+        current.input.prop('disabled', true);
+      }
+      current.check.on('click', function(){
+        if ( current.input.prop('disabled') ) {
+          console.log('enable');
+          current.input.prop('disabled', false);
+        } else {
+          console.log('disable');
+          current.input.prop('disabled', true);
+        }
+      });
+    });
+  }
 });
